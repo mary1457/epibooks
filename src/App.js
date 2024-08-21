7a5ca3f9c6e0370fba1be9a8faf2bd0800add2f2
mary1457 <piabaldoin@gmail.com>
2024-08-21 08:21:48 +0200

    modifiche

--- src/App.js
+++ src/App.js
@@ -1,25 +1,29 @@
-import logo from './logo.svg';
-import './App.css';
+
+
+
+import 'bootstrap/dist/css/bootstrap.min.css'
+import './App.css'
+import MyNav from './components/MyNav'
+import Welcome from './components/Welcome'
+import AllTheBooks from './components/AllTheBooks'
+import MyFooter from './components/MyFooter'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <>
+      <header>
+        <MyNav />
       </header>
-    </div>
-  );
+      <main> 
+        <Welcome />
+        <AllTheBooks />
+      </main>
+      <footer>
+      <MyFooter />
+      </footer>
+     
+    </>
+  )
 }
 
-export default App;
+export default App
---
+++ src/components/AllTheBooks.jsx
@@ -0,0 +1,28 @@
+
+
+import { Card, Col, Row , Button} from 'react-bootstrap'
+import fantasy from '../books/fantasy.json'
+const AllTheBooks = () => {
+  return (
+    <Row className="g-2">
+      {fantasy.map((book) => {
+        return (
+          <Col xs={12} md={6} lg={4} xl={2} key={book.asin}>
+            <Card className="book-cover d-flex flex-column">
+            <Card.Img variant="top" src={book.img} />
+                    <Card.Body>
+                      <Card.Title>{book.title}</Card.Title>
+                      <Card.Text>
+                      {book.category}
+                      </Card.Text>
+                      <Button variant="primary"> {book.price}</Button>
+                    </Card.Body>
+            </Card>
+          </Col>
+        )
+      })}
+    </Row>
+  )
+}
+
+export default AllTheBooks
